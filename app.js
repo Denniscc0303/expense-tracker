20912f42ee3b7ab91713446faae2f7b5d9c711ee
Denniscc <tonynooy@gmail.com>
2021-02-04 19:08:06 +0800

    feat: modify index.hbs

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
 const express = require('express')
 const exphbs = require('express-handlebars');
-
+const Record = require('./models/record')
 const app = express()
 const PORT = 3000
+require('./config/mongoose')
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
+//使用public靜態資料
+app.use(express.static('public'))
 
 
 app.get('/', (req, res) => {
-  res.render('index')
+  Record.find()
+    .lean()
+    .then(records => {
+      res.render('index', { records })
+    })
+    .catch(error => console.error(error))
 })
 
 app.listen(PORT, () => {
   console.log(`App is running on http://localhost:${PORT}`)
 })
+
--- models/seeds/recordSeeder.js
+++ models/seeds/recordSeeder.js
@@ -1,10 +1,10 @@
 const Record = require('../record')
-const recordList = require('../seeds/record.json').results
+const recordList = require('./record.json')
 const db = require('../../config/mongoose')
 
 db.once('open', () => {
   console.log('mongodb connected!')
-  recordList.forEach(record => Record.create(record))
+  recordList.results.forEach(record => Record.create(record))
   console.log('done')
 })
 
