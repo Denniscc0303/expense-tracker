cb448b95db613d7a6ed553f5927ee4dbab3910cd
Denniscc <tonynooy@gmail.com>
2021-02-05 19:05:41 +0800

    feat: fix npm run seed script

--- models/seeds/categorySeeder.js
+++ models/seeds/categorySeeder.js
@@ -4,13 +4,7 @@ const db = require('../../config/mongoose')
 
 db.once('open', () => {
   console.log('mongodb connected!')
-  categoryList.forEach(category => Category.create(category))
+  categoryList.forEach(category => Category.create(category)
+    .then(() => { return db.close() }))
   console.log('done')
 })
-
-
-
-
-
-
-
